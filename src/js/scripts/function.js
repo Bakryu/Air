c75407fa1cbcca4774ad3e3843c6562743eb6724
Stanislav Bakryu <stasbakryu@gmail.com>
2020-09-10 11:34:57 +0300

    second section

--- src/js/scripts/function.js
+++ src/js/scripts/function.js
@@ -1,5 +1,9 @@
-const fn = (...args) => {
-  const res = 123123;
-  args.forEach((item) => console.log(item));
-};
-export default fn;
+$('.hero__slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+
+    prevArrow: '<div class="slider__arrow slider__arrow-left"><img class="slider__arrow-img" src="./images/arrow-left.png" alt=""></div>',
+    nextArrow: '<div class="slider__arrow slider__arrow-right"><img class="slider__arrow-img" src="./images/arrow-right.png" alt=""></div>',
+
+});
